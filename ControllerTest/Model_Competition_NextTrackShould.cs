75390b0dcf4b011b20ee4bc5d73e00c15bd7cefe
krijn <grimmekrijn@gmail.com>
2022-09-21 23:03:14 +0200

    Level 3-1 Unit Test Added

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Model;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    public class Model_Competition_NextTrackShould
+    {
+        #region Attributes
+
+        private Competition _competition;
+
+        #endregion
+
+        #region Methods
+
+        [SetUp]
+        public void SetUp()
+        {
+            _competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            Track result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+
+        #endregion
+    }
+}
+
