2356cde80a155ffcb9e54a903e8fb979a73da23e
krijn <grimmekrijn@gmail.com>
2022-09-27 23:17:13 +0200

    Level 3-4

--- ControllerTest/Model_Competition_NextTrackShould.cs
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -36,6 +36,29 @@ namespace ControllerTest
             Assert.That(track, Is.EqualTo(result));
         }
 
+        [Test]
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            Track track = new Track("test", new SectionTypes[] {SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.RightCorner});
+            _competition.Tracks.Enqueue(track);
+            Track result = _competition.NextTrack();
+            result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NextTrack_TwoInQueue_ReturnFirstTrack()
+        {
+            Track track1 = new Track("test1", new SectionTypes[] {SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.RightCorner});
+            Track track2 = new Track("test2", new SectionTypes[] {SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.RightCorner});
+            _competition.Tracks.Enqueue(track1);
+            _competition.Tracks.Enqueue(track2);
+            Track result = _competition.NextTrack();
+            Assert.That(track1, Is.EqualTo(result));
+            result = _competition.NextTrack();
+            Assert.That(track2, Is.EqualTo(result));
+        }
+        
         #endregion
     }
 }
