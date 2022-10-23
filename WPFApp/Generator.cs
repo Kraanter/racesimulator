c119c8b21c14571d99cbb18984b995678b3c5efd
Kraanter <krijngrimme@hotmail.com>
2022-10-23 22:07:34 +0200

    Working on Level 6-4

--- WPFApp/Generator.cs
+++ WPFApp/Generator.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace WPFApp
 {
@@ -26,7 +29,52 @@ namespace WPFApp
 
         public static void clear()
         {
-            Images.Clear();
+            Images?.Clear();
+        }
+        
+        public static Bitmap CreateBitmap(int Width, int Height)
+        {
+            string key = Width + "x" + Height;
+            if (Images.ContainsKey(key))
+                return Images[key];
+            Bitmap image = GetBitmap(key);
+            Bitmap newImage = new Bitmap(Width, Height);
+            Graphics g = Graphics.FromImage(newImage);
+            g.DrawImage(image, 0, 0, Width, Height);
+            return newImage;
+        }
+        
+        public static BitmapSource CreateBitmapSourceFromGdiBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            var bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            try
+            {
+                var size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
         }
     }
 }
