81823ab07d0c100de52a3bdd88ceb91b81d136da
Kraanter <krijngrimme@hotmail.com>
2022-10-28 18:15:29 +0200

    Extra

--- WPFApp/Display.cs
+++ WPFApp/Display.cs
@@ -124,6 +124,25 @@ public static class Display
         });
         return image;
     }
+    public static Bitmap RotateImage(Bitmap b, float angle)
+    {
+        //create a new empty bitmap to hold rotated image
+        Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
+        //make a graphics object from the empty bitmap
+        using (Graphics g = Graphics.FromImage(returnBitmap))
+        {
+            //move rotation point to center of image
+            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+            //rotate
+            g.RotateTransform(angle);
+            //move image back
+            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+            // draw passed in image onto graphics object
+            g.DrawImage(b, new Point(0, 0));
+        }
+
+        return returnBitmap;
+    }
 
     public static Bitmap AddParticpants(Bitmap original, Section section)
     {
@@ -164,25 +183,7 @@ public static class Display
     }
 
 
-    public static Bitmap RotateImage(Bitmap b, float angle)
-    {
-        //create a new empty bitmap to hold rotated image
-        Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
-        //make a graphics object from the empty bitmap
-        using (Graphics g = Graphics.FromImage(returnBitmap))
-        {
-            //move rotation point to center of image
-            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
-            //rotate
-            g.RotateTransform(angle);
-            //move image back
-            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
-            // draw passed in image onto graphics object
-            g.DrawImage(b, new Point(0, 0));
-        }
-
-        return returnBitmap;
-    }
+    
 
     public static void Initialize()
     {
